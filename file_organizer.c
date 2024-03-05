a76d8996690c280edf2c5ec42aefcef89611fbed
CrisianFG <147004159+CrisianFG@users.noreply.github.com>
2024-03-04 22:52:18 -0600

    Fixed issues in get_file_extension() and get_user()

--- file_organizer.c
+++ file_organizer.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 const char *get_user(void);
 const char *get_file_extension(const char *file);
@@ -10,38 +12,58 @@ int main (int argc, char **argv) {
     const char *user = get_user();
     char *dir_list[MAX_PATH + 1];
 
-    printf("%s", get_file_extension("something"));
+    printf("%s", get_file_extension("something.txt"));
 
     return 0;
 }
 
 const char *get_user(void) {
-    char *username = NULL;
-    const DWORD username_length = 256 + 1;
-    username = GetUserName(username, &username_length);
+    char *username = malloc(sizeof(char) * (256 + 1));
+    if (username == NULL) {
+        printf("Memory allocation for user failed.\n");
 
-    if (username != NULL) {
-        const char *username = username;
+        return NULL;
+    }
+
+    DWORD username_length = sizeof(username);
+
+    if (GetUserName(username, &username_length)) { 
         return username;
     } else {
-        perror("User could not be found. Please try again.");
+        free(username);
+        printf("User could not be found. Please try again.\n");
+
+        return NULL;
     }
 }
 
 void organizeFile(const char* file_dir) {
-    printf("Organizing files...");
-    printf("Files organized!");
+    printf("Organizing files...\n");
+    printf("Files organized!\n");
 }
 
 const char *get_file_extension(const char *file) {
-    const char *extension = strrchr(file, ".");
-    const char *file_extension = malloc(strlen(extension));
+    if (!is_valid_file) {
+        printf("File is not valid. Please try again.\n");
 
-    if (!extension || extension == file) {
-        free(file_extension);
-        perror("Extension is not valid.");
-    } else {
-        file_extension = strcpy(file_extension, extension);
-        return file_extension;
+        return NULL;    
     }
+
+    const char *extension = strrchr(file, '.');
+
+    if (!extension) {
+        printf("Extension not found.\n");
+
+        return NULL;
+    } else if (extension == file) {
+        printf("File name starts with '.'. Make sure it has a name before the extension.\n");
+
+        return NULL;
+    } else {
+        return extension;
+    }
+}
+
+int is_valid_file(const char *file_dir) {
+
 }
--- file_organizer.py
+++
@@ -1,9 +0,0 @@
-import os
-import getpass
-import shutil
-
-user = getpass.getuser()
-directory = fr"C:\Users\{user}\Documents"
-
-def organize_file(file):
-    print("Dir: ")
